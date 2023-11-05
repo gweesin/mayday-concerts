13899f8d454a5858f2b1294e82be07c30760a926
GweesinChan <gweesin2000@gmail.com>
2023-11-05 22:33:57 +0800

    feat: support PWA(#1)

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.svg", "mask-icon.svg"],
+      manifest: {
+        name: "Mayday Concerts",
+        short_name: "MC",
+        description:
+          "This App is created to provide an offline Progressive Web App (PWA) for viewing lyrics during Mayday concerts, to prevent difficulties in reading lyrics subtitles when the screen is too far away.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.svg",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.svg",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
